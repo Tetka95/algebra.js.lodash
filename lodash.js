28e69d807d67859de11c6d4e73b903ccd734ff68
Tetka95 <martina.bobek95@gmail.com>
2024-05-02 17:40:00 +0200

    .

--- lodash.js
+++ lodash.js
@@ -1,1 +1,24 @@
+//Load the full build.
+var _ = require('lodash');
 
+//1.
+
+var dobro = "a1234567890";
+var lose = "1123456";
+var jedinstveniZnakovi = function (password) {
+    for (let i = 0; i <= password.length; i++) {
+        var numToCheck = password.charAt(i);
+        //console.log(numToCheck + "is integer = " + isBroj(numToCheck));
+        if(!isBroj(numToCheck)) {
+            console.log(numToCheck + " nije broj");
+            break;
+        }
+    }
+};
+
+function isBroj(broj) {
+    return !isNaN(broj);
+}
+
+jedinstveniZnakovi(dobro);
+jedinstveniZnakovi(lose);

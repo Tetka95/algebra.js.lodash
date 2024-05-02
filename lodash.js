e4470e165fcaac0fc2b804ad76f391425a74d14f
Tetka95 <martina.bobek95@gmail.com>
2024-05-02 18:36:19 +0200

    dodane funkcije

--- lodash.js
+++ lodash.js
@@ -3,22 +3,58 @@ var _ = require('lodash');
 
 //1.
 
-var dobro = "a1234567890";
+var dobro = "12345678901234567890";
 var lose = "1123456";
 var jedinstveniZnakovi = function (password) {
-    for (let i = 0; i <= password.length; i++) {
+    var mojNiz = [];
+    console.log("password.length = " + password.length);
+    for (let i = 0; i < password.length; i++) {
         var numToCheck = password.charAt(i);
         //console.log(numToCheck + "is integer = " + isBroj(numToCheck));
         if(!isBroj(numToCheck)) {
             console.log(numToCheck + " nije broj");
-            break;
+            return false;
+        }
+        if (mojNiz.includes(numToCheck)) {
+            console.log(numToCheck + " već postoji u našem nizu.");
+            return false;
+        } else {
+            console.log(numToCheck + " NE postoji u našem nizu.");
+            mojNiz.push(numToCheck);
         }
     }
+    return true;
 };
 
 function isBroj(broj) {
     return !isNaN(broj);
 }
 
+function isMoreThan10(password) {
+    if (password.length > 10) {
+        return true;
+    }
+    return false;
+}
+
+function skratiNa10(password) {
+    if (password.length <= 10) return password;
+    /*var tmpPassword = "";
+    for (let i = 0; i < 10; i++) {
+        var charAtIndex = password.charAt(i);
+        tmpPassword += charAtIndex;
+    }
+    return tmpPassword;*/
+    return password.substring(0, 10); //brža metoda od if petlje
+}
+
 jedinstveniZnakovi(dobro);
+console.log("------");
 jedinstveniZnakovi(lose);
+
+console.log("Password je ispravan: " + jedinstveniZnakovi(dobro));
+console.log("Password je ispravan: " + jedinstveniZnakovi(lose));
+console.log("Password ima više od 10 znakova " + isMoreThan10(dobro));
+console.log("Password ima više od 10 znakova " + isMoreThan10(lose));
+console.log("Password nakon skracivanja: " + skratiNa10(dobro));
+console.log("Password nakon skracivanja: " + skratiNa10(lose));
